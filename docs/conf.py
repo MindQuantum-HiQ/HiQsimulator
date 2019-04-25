826dfa84145c9de905e4097207ecc032978932b3
Huawei HiQ <hiqinfo@huawei.com>
2019-04-25 18:05:43 +0800

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -39,7 +39,15 @@ extensions = [
 
 autosummary_generate = True
 
-autodoc_mock_imports = ["mpi4py"]
+from unittest.mock import MagicMock
+
+class Mock(MagicMock):
+    @classmethod
+    def __getattr__(cls, name):
+        return MagicMock()
+
+MOCK_MODULES = ['mpi4py']
+sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
