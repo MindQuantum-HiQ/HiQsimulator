4bd271b8a7c93aedb6f31fd968efb98135482a30
Huawei HiQ <hiqinfo@huawei.com>
2019-04-25 18:20:25 +0800

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -21,6 +21,17 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('../hiq'))
 
+# Mock mpi4py to get around having to install it on RTD server (which fails)
+from unittest.mock import MagicMock
+
+class Mock(MagicMock):
+    @classmethod
+    def __getattr__(cls, name):
+        return MagicMock()
+
+MOCK_MODULES = ['mpi4py']
+sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
+
 # -- General configuration ------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
@@ -38,16 +49,6 @@ extensions = [
 ]
 
 autosummary_generate = True
-
-from unittest.mock import MagicMock
-
-class Mock(MagicMock):
-    @classmethod
-    def __getattr__(cls, name):
-        return MagicMock()
-
-MOCK_MODULES = ['mpi4py']
-sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
