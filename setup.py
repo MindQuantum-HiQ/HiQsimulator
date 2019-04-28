1030d23f7417db63c9e5382bea00da41ab8edd45
Huawei HiQ <hiqinfo@huawei.com>
2019-04-28 18:21:50 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -31,20 +31,6 @@ def get_install_requires():
 
     return requirements
 
-class get_pybind_include(object):
-    """Helper class to determine the pybind11 include path
-
-    The purpose of this class is to postpone importing pybind11
-    until it is actually installed, so that the ``get_include()``
-    method can be invoked. """
-
-    def __init__(self, user=False):
-        self.user = user
-
-    def __str__(self):
-        import pybind11
-        return pybind11.get_include(self.user)
-
 
 class CMakeExtension(Extension):
     def __init__(self, target, name, sourcedir=''):
