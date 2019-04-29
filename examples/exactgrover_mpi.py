0589d411025a202e592675443f84f9cf7fdec347
Huawei HiQ <hiqinfo@huawei.com>
2019-04-29 14:01:39 +0800

    Update exactgrover_mpi.py

--- examples/exactgrover_mpi.py
+++ examples/exactgrover_mpi.py
@@ -129,7 +129,7 @@ def alternating_bits_oracle_modified(eng, qubits, output, phi):
 
 if __name__ == "__main__":
     # create a main compiler engine with a simulator backend:
-    backend = SimulatorMPI(gate_fusion=True)
+    backend = SimulatorMPI(gate_fusion=True, num_local_qubits=20)
     # backend = CircuitDrawer()
     # locations = {}
     # for i in range(module.nqubits):
