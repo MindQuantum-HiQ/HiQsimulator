a56719ee3c8babbe43ec1d7a13535e69adc24fa0
Huawei HiQ <hiqinfo@huawei.com>
2019-05-09 16:41:41 +0800

    Update SymMatrixSolver_mpi.py

--- examples/SymMatrixSolver_mpi.py
+++ examples/SymMatrixSolver_mpi.py
@@ -287,7 +287,7 @@ if __name__ == "__main__":
     		   TagRemover(),
     		   LocalOptimizer(cache_depth),
     		   #CommandPrinter(),
-           GreedyScheduler()
+    		   GreedyScheduler()
     		   ]
     
     eng = HiQMainEngine(backend, engines) 
