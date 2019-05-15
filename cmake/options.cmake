bb12820455fc6c18b23c2f37ac7af0617cbdee65
Nguyen Damien <damien1@huawei.com>
2019-05-15 22:41:48 +0200

    Introduce clang-format option in CMake

--- cmake/options.cmake
+++ cmake/options.cmake
@@ -3,8 +3,13 @@ include(CMakeDependentOption)
 # ------------------------------------------------------------------------------
 
 option(USE_INTRIN "Enable/disable the use of intrinsics" ON)
-cmake_dependent_option(USE_INTRIN_BUFFER "Using intrinsics with buffer" OFF
-  "USE_INTRIN" OFF)
+cmake_dependent_option(USE_INTRIN_BUFFER
+                       "Using intrinsics with buffer"
+                       OFF
+                       "USE_INTRIN"
+                       OFF)
+
+option(USE_CLANG_FORMAT "Setup clangformat target" OFF)
 
 # ==============================================================================
 
@@ -16,4 +21,13 @@ if(USE_INTRIN)
   endif()
 else(USE_INTRIN)
   add_definitions(-DNOINTRIN)
+endif()
+
+# ==============================================================================
+
+if(USE_CLANG_FORMAT)
+  include(ClangFormat)
+
+  file(GLOB_RECURSE clangformat_srcs LIST_DIRECTORIES FALSE "*.hpp" "*.cpp")
+  clangformat_setup("${clangformat_srcs}")
 endif()
