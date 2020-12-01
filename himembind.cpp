666a75b6a1e01a037b2715e9df1305d144171918
Yi Liu <11610522@mail.sustech.edu.cn>
2020-12-01 11:09:22 +0800

    Rewrite with a cpp style
    Refactor patch

--- himembind.cpp
+++ himembind.cpp
@@ -22,22 +22,15 @@
 #endif  // _MSC_VER
 
 #include <hwloc.h>
-#include <stdio.h>
-#include <stdlib.h>
-
 #include <memory>
-#include <string.h>
-#include <ctype.h>
+#include <algorithm>
+#include <cctype>
+#include <string_view>
+#include <iostream>
 
-int is_valid_call_name(const char *call_name, const char *valid_name);
+bool is_valid_call_name(const char *call_name);
 
-int is_valid_character(char character);
-
-int check_single_argument(const char *arg);
-
-int is_valid_arguments(char **args, int argc);
-
-int check_call_name(const char *call_name);
+bool is_valid_arguments(char **args, int argc);
 
 int main(int argc, char **argv) {
     int ret = 0;
@@ -60,7 +53,7 @@ int main(int argc, char **argv) {
 
     char *ss = nullptr;
     hwloc_bitmap_asprintf(&ss, cpuset);
-    fprintf(stderr, "cpuset: %s\n", ss);
+    std::cerr << "cpuset: " << ss << std::endl;
     free(ss);
     ss = nullptr;
 
@@ -68,31 +61,32 @@ int main(int argc, char **argv) {
     hwloc_nodeset_t nodeset = hwloc_bitmap_alloc();
     hwloc_cpuset_to_nodeset(topology, cpuset, nodeset);
     hwloc_bitmap_asprintf(&ss1, nodeset);
-    fprintf(stderr, "nodeset: %s\n", ss1);
+    std::cerr << "nodeset: " << ss1 << std::endl;
+
     free(ss1);
     ss1 = nullptr;
 
     ret = hwloc_set_membind_nodeset(topology, nodeset, HWLOC_MEMBIND_BIND,
                                     HWLOC_MEMBIND_STRICT);
     if (ret) {
-        fprintf(stderr, "%s: Failed to set membind policy\n", myname);
+        std::cerr << myname << ": Failed to set membind policy" << std::endl;
         goto exit_failure;
     }
 
     if (argc == 0) {
-        fprintf(stderr, "Usage: %s <cmd>\n", myname);
+        std::cerr << "Usage: " << myname << " <cmd>" << std::endl;
         goto exit_failure;
     }
 
     // Check for call name
-    if (!check_call_name(callname)) {
-        fprintf(stderr, "Invalid call name\n");
+    if (!is_valid_call_name(callname)) {
+        std::cerr << "Invalid call name" << std::endl;
         goto exit_failure;
     }
 
     // Check for arguments
     if (!is_valid_arguments(argv, argc)) {
-        fprintf(stderr, "Invalid arguments\n");
+        std::cerr << "Invalid arguments" << std::endl;
         goto exit_failure;
     }
 
@@ -100,8 +94,7 @@ int main(int argc, char **argv) {
     ret = execvp(callname, argv);
 
     if (ret) {
-        fprintf(stderr, "%s: Failed to launch executable \"%s\"\n", callname,
-                argv[0]);
+        std::cerr << callname << ": Failed to launch executable \"" << argv[0] << "\"" << std::endl;
         perror("execvp");
         goto exit_failure;
     }
@@ -114,90 +107,35 @@ int main(int argc, char **argv) {
 }
 
 /**
- * Check valid call name
- * @param call_name
- * @param valid_name
- * @return check result
- */
-int is_valid_call_name(const char *call_name, const char *valid_name) {
-    int call_name_len = strlen(call_name);
-    int valid_name_len = strlen(valid_name);
-
-    /**
-     * The two stings should be equal length.
-     */
-    if (call_name_len != valid_name_len)
-        return 0;
-
-    /**
-     * check string content
-     */
-    int i = 0;
-    while (i < call_name_len) {
-        if (call_name[i] != valid_name[i])
-            return 0;
-        i++;
-    }
-
-    return 1;
-}
-
-/**
- * check valid character
- * @param character
- * @return
- */
-int is_valid_character(char character) {
-    if (isalpha(character) || isnumber(character))
-        return 1;
-    /**
-     * check other allowed character '-','.','_','/','\'
-     */
-    if (character == '-' || character == '.' || character == '_' || character == '/' || character == '\')
-        return 1;
-
-    return 0;
-}
-
-/**
- * check whether single argument is valid
- * @param arg
- * @return result
- */
-int check_single_argument(const char *arg) {
-    int len = strlen(arg);
-    int i = 0;
-    while (i < len) {
-        if (!is_valid_character(arg[i]))
-            return 0;
-        i++;
-    }
-    return 1;
-}
-
-/**
- * Check arguments are valid
- * @param args arguments
+ * Check arguments in cmd
+ * @param args arguments vector
  * @param argc arguments count
  * @return result
  */
-int is_valid_arguments(char **args, int argc) {
-    int i = 0;
-    while (i < argc) {
-        if (!check_single_argument(args[i]))
-            return 0;
-        i++;
-    }
-    return 1;
+bool is_valid_arguments(char **args, int argc) {
+    return std::all_of(
+            args, args + argc,
+            [](std::string_view arg) {
+                return std::all_of(std::begin(arg), std::end(arg),
+                                   [](unsigned char c) {
+                                       return std::isalnum(c)
+                                              || c == '-'
+                                              || c == '.'
+                                              || c == '_'
+                                              || c == '/'
+                                              || c == '\\'
+                                              || c == ' ';
+                                   });
+            });
 }
 
 /**
- * Check call name is valid
+ * Check call name in cmd
  * @param call_name
  * @return result
  */
-int check_call_name(const char *call_name) {
-    if (is_valid_call_name(call_name, "mpirun"))
-        return 1;
-    return 0;
+bool is_valid_call_name(const char *call_name) {
+    if (strcmp(call_name, "mpirun") == 0)
+        return true;
+    return false;
 }
