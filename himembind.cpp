5bb16ad40ad19af2cca0bdfe94c979d146a2ca10
Damien Nguyen <damien1@huawei.com>
2020-12-01 10:34:19 +0100

    Introduce RAII for himembind

--- himembind.cpp
+++ himembind.cpp
@@ -22,88 +22,96 @@
 #endif  // _MSC_VER
 
 #include <hwloc.h>
-#include <memory>
+
 #include <algorithm>
 #include <cctype>
-#include <string_view>
 #include <iostream>
+#include <memory>
+#include <string_view>
+
+#include "details/scope_guard.hpp"
 
 bool is_valid_call_name(const char *call_name);
 
 bool is_valid_arguments(char **args, int argc);
 
-int main(int argc, char **argv) {
-    int ret = 0;
+int main(int argc, char **argv)
+{
+     int ret = 0;
 
-    const char *myname = argv[0];
-    ++argv;
-    --argc;
+     const char *myname = argv[0];
+     ++argv;
+     --argc;
 
-    const char *callname = argv[0];
+     const char *callname = argv[0];
 
-    hwloc_topology_t topology;
-    unsigned long flags = 0;
+     hwloc_topology_t topology;
+     unsigned long flags = 0;
 
-    hwloc_topology_init(&topology);
-    hwloc_topology_set_flags(topology, flags);
-    hwloc_topology_load(topology);
+     hwloc_topology_init(&topology);
+     hwloc_topology_set_flags(topology, flags);
+     hwloc_topology_load(topology);
 
-    hwloc_cpuset_t cpuset = hwloc_bitmap_alloc();
-    hwloc_get_cpubind(topology, cpuset, HWLOC_CPUBIND_PROCESS);
+     hwloc_cpuset_t cpuset = hwloc_bitmap_alloc();
+     hwloc_get_cpubind(topology, cpuset, HWLOC_CPUBIND_PROCESS);
 
-    char *ss = nullptr;
-    hwloc_bitmap_asprintf(&ss, cpuset);
-    std::cerr << "cpuset: " << ss << std::endl;
-    free(ss);
-    ss = nullptr;
+     {
+          char *ss = nullptr;
+          auto guard = sg::make_c_pointer_guard(ss);
 
-    char *ss1 = nullptr;
-    hwloc_nodeset_t nodeset = hwloc_bitmap_alloc();
-    hwloc_cpuset_to_nodeset(topology, cpuset, nodeset);
-    hwloc_bitmap_asprintf(&ss1, nodeset);
-    std::cerr << "nodeset: " << ss1 << std::endl;
+          hwloc_bitmap_asprintf(&ss, cpuset);
+          std::cerr << "cpuset: " << ss << std::endl;
+     }
 
-    free(ss1);
-    ss1 = nullptr;
+     hwloc_nodeset_t nodeset = hwloc_bitmap_alloc();
+     hwloc_cpuset_to_nodeset(topology, cpuset, nodeset);
+     {
+          char *ss1 = nullptr;
+          auto guard = sg::make_c_pointer_guard(ss1);
+          hwloc_bitmap_asprintf(&ss1, nodeset);
+          std::cerr << "nodeset: " << ss1 << std::endl;
+     }
 
-    ret = hwloc_set_membind_nodeset(topology, nodeset, HWLOC_MEMBIND_BIND,
-                                    HWLOC_MEMBIND_STRICT);
-    if (ret) {
-        std::cerr << myname << ": Failed to set membind policy" << std::endl;
-        goto exit_failure;
-    }
+     auto guard = sg::make_scope_guard([&cpuset, &nodeset, &topology]() {
+          hwloc_bitmap_free(cpuset);
+          hwloc_bitmap_free(nodeset);
+          hwloc_topology_destroy(topology);
+     });
 
-    if (argc == 0) {
-        std::cerr << "Usage: " << myname << " <cmd>" << std::endl;
-        goto exit_failure;
-    }
+     ret = hwloc_set_membind_nodeset(topology, nodeset, HWLOC_MEMBIND_BIND,
+                                     HWLOC_MEMBIND_STRICT);
 
-    // Check for call name
-    if (!is_valid_call_name(callname)) {
-        std::cerr << "Invalid call name" << std::endl;
-        goto exit_failure;
-    }
+     if (ret) {
+          std::cerr << myname << ": Failed to set membind policy" << std::endl;
+          return EXIT_FAILURE;
+     }
 
-    // Check for arguments
-    if (!is_valid_arguments(argv, argc)) {
-        std::cerr << "Invalid arguments" << std::endl;
-        goto exit_failure;
-    }
+     if (argc == 0) {
+          std::cerr << "Usage: " << myname << " <cmd>" << std::endl;
+          return EXIT_FAILURE;
+     }
 
+     // Check for call name
+     if (!is_valid_call_name(callname)) {
+          std::cerr << "Invalid call name" << std::endl;
+          return EXIT_FAILURE;
+     }
 
-    ret = execvp(callname, argv);
+     // Check for arguments
+     if (!is_valid_arguments(argv, argc)) {
+          std::cerr << "Invalid arguments" << std::endl;
+          return EXIT_FAILURE;
+     }
 
-    if (ret) {
-        std::cerr << callname << ": Failed to launch executable \"" << argv[0] << "\"" << std::endl;
-        perror("execvp");
-        goto exit_failure;
-    }
+     ret = execvp(callname, argv);
 
-    exit_failure:
-    hwloc_bitmap_free(cpuset);
-    hwloc_bitmap_free(nodeset);
-    hwloc_topology_destroy(topology);
-    return EXIT_FAILURE;
+     if (ret) {
+          std::cerr << callname << ": Failed to launch executable \"" << argv[0]
+                    << "\"" << std::endl;
+          perror("execvp");
+          return EXIT_FAILURE;
+     }
+     return 0;
 }
 
 /**
@@ -112,21 +120,15 @@ int main(int argc, char **argv) {
  * @param argc arguments count
  * @return result
  */
-bool is_valid_arguments(char **args, int argc) {
-    return std::all_of(
-            args, args + argc,
-            [](std::string_view arg) {
-                return std::all_of(std::begin(arg), std::end(arg),
-                                   [](unsigned char c) {
-                                       return std::isalnum(c)
-                                              || c == '-'
-                                              || c == '.'
-                                              || c == '_'
-                                              || c == '/'
-                                              || c == '\\'
-                                              || c == ' ';
-                                   });
-            });
+bool is_valid_arguments(char **args, int argc)
+{
+     return std::all_of(args, args + argc, [](std::string_view arg) {
+          return std::all_of(
+              std::begin(arg), std::end(arg), [](unsigned char c) {
+                   return std::isalnum(c) || c == '-' || c == '.' || c == '_'
+                          || c == '/' || c == '\\' || c == ' ';
+              });
+     });
 }
 
 /**
@@ -134,12 +136,13 @@ bool is_valid_arguments(char **args, int argc) {
  * @param call_name
  * @return result
  */
-bool is_valid_call_name(const char *call_name) {
-    if (strcmp(call_name, "mpirun") == 0)
-        return true;
-    if (strcmp(call_name, "python") == 0)
-        return true;    
-    if (strcmp(call_name, "python3") == 0)
-        return true;
-    return false;
+bool is_valid_call_name(const char *call_name)
+{
+     if (strcmp(call_name, "mpirun") == 0)
+          return true;
+     if (strcmp(call_name, "python") == 0)
+          return true;
+     if (strcmp(call_name, "python3") == 0)
+          return true;
+     return false;
 }
